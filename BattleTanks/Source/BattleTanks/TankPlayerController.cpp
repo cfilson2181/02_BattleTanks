bc44f7c26cba22536cb55c37cc12a4b9b5352529
DESKTOP-L5JDJK7\chunk <chunk8110@gmail.com>
2018-05-02 18:33:21 -0700

    Added understanding of override and virtual keyword functions in C++. Created a sub BeginPlay() function that piggybacks off of AActor::BeginPlay

--- BattleTanks/Source/BattleTanks/TankPlayerController.cpp
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -2,6 +2,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Get the controlled tank pawn. Log the controller name if it exists at begin play
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller possessing %s"), *GetControlledTank()->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Player controller not possessing tank"));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
